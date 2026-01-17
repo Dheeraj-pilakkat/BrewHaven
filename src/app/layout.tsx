9e86876ca14a47d0c808bcf3ef1d822a1c0f998e
Dheeraj <pilakkatdheeraj@gmail.com>
2026-01-17 14:51:29 +0530

    feat: Add Vercel Analytics dependency and component to the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body className="antialiased font-inter">
         <CartProvider>
           {children}
+          <Analytics />
         </CartProvider>
       </body>
     </html>
